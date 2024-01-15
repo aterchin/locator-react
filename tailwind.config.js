1bf95e6028bb76843e8cc4d0317233bac42f6754
Adam Terchin <adam.terchin@gmail.com>
2024-01-15 09:38:28 -0500

    tailwind default colors, heroicons/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import colors from '@tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -14,6 +15,17 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'brand-green': {
+                    200: '#2e4237',
+                    DEFAULT: '#029840',
+                    light: '#9fffc7',
+                },
+                'brand-orange': {
+                    DEFAULT: '#a23c1a',
+                    light: '#efbeae',
+                },
             },
         },
     },
