5693c92a634730cef83ebe064f81810d3f92a5a0
Adam Terchin <adam.terchin@gmail.com>
2024-01-15 09:19:07 -0500

    resource to transform json output (may need)   - php artisan make:resource BusinessResource

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BusinessResource;
 use App\Models\Business;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,10 @@ class BusinessController extends Controller
      */
     public function index()
     {
-        //
+        return BusinessResource::collection(
+            Business::orderBy('created_at', 'desc')
+                ->paginate(5)
+        );
     }
 
     /**
