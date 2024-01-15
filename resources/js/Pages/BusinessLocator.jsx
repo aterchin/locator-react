e2dd66cc67f7086650167406a755799a5ef507e1
Adam Terchin <adam.terchin@gmail.com>
2024-01-15 09:01:12 -0500

    home screen jsx to go with route from last commit

---
+++ resources/js/Pages/BusinessLocator.jsx
@@ -0,0 +1,17 @@
+import { Head } from "@inertiajs/react";
+
+const BusinessLocator = () => {
+    return (
+        <>
+            <Head title="Business Locator | Laravel/React" />
+            <h1>Welcome</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloremque eveniet mollitia eaque alias vitae expedita
+                voluptatibus ipsa. Quidem animi accusantium voluptas ullam odio
+                similique, pariatur numquam dolor mollitia. Accusamus, impedit.
+            </p>
+        </>
+    );
+};
+export default BusinessLocator;
