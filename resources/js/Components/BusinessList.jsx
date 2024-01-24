e94856d449633e409f5f951d9062f25669d3fbdb
Adam Terchin <adam.terchin@gmail.com>
2024-01-24 10:03:26 -0500

    scroll to active listing on marker click

--- resources/js/Components/BusinessList.jsx
+++ resources/js/Components/BusinessList.jsx
@@ -1,14 +1,52 @@
+import { useRef } from 'react';
 import BusinessListItem from './BusinessListItem';
 
 const BusinessList = ({ businesses, active, onToggleActive }) => {
+    const itemsRef = useRef(null);
+
+    //react.dev/learn/manipulating-the-dom-with-refs#how-to-manage-a-list-of-refs-using-a-ref-callback
+    function scrollToId(itemId) {
+        const map = getMap();
+        const node = map.get(itemId);
+        node.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center',
+        });
+    }
+
+    function getMap() {
+        if (!itemsRef.current) {
+            // Initialize the Map on first usage.
+            itemsRef.current = new Map();
+        }
+        return itemsRef.current;
+    }
+
     const renderedItems = businesses.map((business) => {
+        if (active === business.id) {
+            scrollToId(business.id);
+        }
+
         return (
-            <BusinessListItem
+            <div
+                className="first:mt-0 mt-3"
                 key={business.id}
-                business={business}
-                active={active}
-                onToggleActive={onToggleActive}
-            />
+                ref={(node) => {
+                    const map = getMap();
+                    if (node) {
+                        map.set(business.id, node);
+                    } else {
+                        map.delete(business.id);
+                    }
+                }}
+            >
+                <BusinessListItem
+                    business={business}
+                    active={active}
+                    onToggleActive={onToggleActive}
+                />
+            </div>
         );
     });
 
